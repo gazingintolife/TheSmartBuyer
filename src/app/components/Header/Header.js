123bda091ac94a65abfd5c65b2e3288e28c66607
hamzashahab1610 <hamzashahab1610@gmail.com>
2021-09-22 14:04:35 +0500

    signin page

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import Logo from "../../assets/logo.svg";
 
 const Header = () => (
-  <header className="header-container">
-    <Link to="/dashboard">SignUp</Link>
+	<header className="header-container">
+		<img className="brand" src={Logo} alt="" />
 
-    <nav>
-      <Link to="/account">My Account</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/">logout</Link>
-    </nav>
+		<nav className="navigation">
+			<Link className="white-btn" to="/account">
+				My Account
+			</Link>
+			<Link className="white-btn" to="/contact">
+				Contact Us
+			</Link>
+			<Link className="green-btn" to="/dashboard">
+				Sign Up
+			</Link>
+			<Link className="green-btn" to="/">
+				Log out
+			</Link>
+		</nav>
 
-    <hr />
-  </header>
+		<hr />
+	</header>
 );
 
 export default Header;
