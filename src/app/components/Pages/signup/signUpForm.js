3ca68e95b0a6ff90c8ca07ca4ab5f469a569c2e3
hamzashahab1610 <hamzashahab1610@gmail.com>
2021-09-22 22:46:45 +0500

    signup page

--- src/app/components/Pages/signup/signUpForm.js
+++ src/app/components/Pages/signup/signUpForm.js
@@ -1,94 +1,108 @@
 import React from "react";
+import "./signup.scss";
 
 class SignUpForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            mobile: '',
-            password: '',
-            error: ''
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: "",
+			lastName: "",
+			mobile: "",
+			password: "",
+			error: "",
+		};
+	}
 
-    onFirstNameChange = (e) => {
-        const firstName = e.target.value;
-        this.setState(() => ({firstName:firstName}))
-    }
+	onFirstNameChange = (e) => {
+		const firstName = e.target.value;
+		this.setState(() => ({ firstName: firstName }));
+	};
 
-    onLastNameChange = (e) => {
-        const lastName = e.target.value;
-        this.setState(() => ({lastName:lastName}))
-    }
+	onLastNameChange = (e) => {
+		const lastName = e.target.value;
+		this.setState(() => ({ lastName: lastName }));
+	};
 
-    onmobileChange = (e) => {
-        const mobile = e.target.value;
-        this.setState(() => ({mobile:mobile}))
-    }
+	onmobileChange = (e) => {
+		const mobile = e.target.value;
+		this.setState(() => ({ mobile: mobile }));
+	};
 
-    onPasswordChange = (e) => {
-        const password = e.target.value;
-        this.setState(() => ({password:password}))
-    }
+	onPasswordChange = (e) => {
+		const password = e.target.value;
+		this.setState(() => ({ password: password }));
+	};
 
-    onSubmit = (e) => {
-        e.preventDefault();
+	onSubmit = (e) => {
+		e.preventDefault();
 
-        if(!this.state.firstName || !this.state.lastName || !this.state.mobile || !this.state.password){
-            this.setState(() => ({error: 'Please fill all the fields'}));
-        }
+		if (
+			!this.state.firstName ||
+			!this.state.lastName ||
+			!this.state.mobile ||
+			!this.state.password
+		) {
+			this.setState(() => ({ error: "Please fill all the fields" }));
+		} else {
+			this.setState(() => ({ error: "" }));
+			this.props.onSubmit({
+				firstName: this.state.firstName,
+				lastName: this.state.lastName,
+				mobile: this.state.mobile,
+				password: this.state.password,
+			});
+			this.setState(() => ({
+				firstName: "",
+				lastName: "",
+				mobile: "",
+				password: "",
+			}));
+		}
+	};
 
-        else {
-            this.setState(() => ({error: ''}));
-            this.props.onSubmit({
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                mobile: this.state.mobile,
-                password: this.state.password
-            });
-            this.setState(() => ({
-                firstName: '',
-                lastName: '',
-                mobile: '',
-                password: ''
-            }))
-        }
-    }
+	render() {
+		return (
+			<div className="signup-right">
+				<h4 className="signup-head">
+					Let’s Make your life a little Smoother
+				</h4>
+				<form className="signup-form" onSubmit={this.onSubmit}>
+					<div className="names-input">
+						<input
+							className="signup-input-element"
+							type="text"
+							placeholder="First Name"
+							value={this.state.firstName}
+							onChange={this.onFirstNameChange}
+						/>
+						<input
+							className="signup-input-element"
+							type="text"
+							placeholder="Last Name"
+							value={this.state.lastName}
+							onChange={this.onLastNameChange}
+						/>
+					</div>
 
-    render(){
-        return(
-            <div>
-                <form onSubmit = {this.onSubmit}>
-                    <input
-                        type = 'text'
-                        placeholder = 'First Name'
-                        value = {this.state.firstName}
-                        onChange = {this.onFirstNameChange}
-                    />
-                    <input
-                        type = 'text'
-                        placeholder = 'Last Name'
-                        value = {this.state.lastName}
-                        onChange = {this.onLastNameChange}
-                    />
-                    <input
-                        type = 'number'
-                        placeholder = 'Phone Number'
-                        value = {this.state.mobile}
-                        onChange = {this.onmobileChange}
-                    />
-                    <input
-                        type = 'text'
-                        placeholder = 'Enter Your Password'
-                        value = {this.state.password}
-                        onChange = {this.onPasswordChange}
-                    />
-                    <button>Get Me Groceries</button>
-                </form>
-            </div>
-        )
-    }
+					<input
+						className="signup-input-element"
+						type="number"
+						placeholder="Phone Number"
+						value={this.state.mobile}
+						onChange={this.onmobileChange}
+					/>
+					<input
+						className="signup-input-element"
+						type="text"
+						placeholder="Enter Your Password"
+						value={this.state.password}
+						onChange={this.onPasswordChange}
+					/>
+					<button className="signup-green-btn">Sign Up</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignUpForm;
--- src/app/components/Pages/signup/signup.js
+++ src/app/components/Pages/signup/signup.js
@@ -1,65 +1,81 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { userLoggedIn } from '../../../actions/signup';
-import SignUpForm from './signUpForm';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { userLoggedIn } from "../../../actions/signup";
+import SignUpForm from "./signUpForm";
+import "./signup.scss";
+import File from "../../../assets/File.svg";
+import Truck from "../../../assets/Truck.svg";
+import Arrow from "../../../assets/Arrow.svg";
 
-const axios = require('axios');
+const axios = require("axios");
 
 class Signup extends React.Component {
-    constructor(props){
-        super(props);
-    }    
+	constructor(props) {
+		super(props);
+	}
 
-    onSignUpSubmit = (user) => {
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+	onSignUpSubmit = (user) => {
+		const options = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-        axios.post('/api/v1/customer/signup', user, options)
-        .then(response => {
-            this.props.userLoggedOn({
-                _id: response.data.body._id,
-                firstName: response.data.body.firstName,
-                lastName: response.data.body.lastName
-            })
-            console.log(response)
-        })
-        .then(() => {
-            console.log(this.props.firstName, this.props._id)
-            return (
-                this.props.history.push(`/dashboard/${this.props._id}`)
-            );
-        }
-            
-        )
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-    render(){
-        return (
-            <div>
-                <SignUpForm onSubmit = {this.onSignUpSubmit}/>
-                <Link to = "/login">Login</Link>
-            </div>
-            )
-    }
+		axios
+			.post("/api/v1/customer/signup", user, options)
+			.then((response) => {
+				this.props.userLoggedOn({
+					_id: response.data.body._id,
+					firstName: response.data.body.firstName,
+					lastName: response.data.body.lastName,
+				});
+				console.log(response);
+			})
+			.then(() => {
+				console.log(this.props.firstName, this.props._id);
+				return this.props.history.push(`/dashboard/${this.props._id}`);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	render() {
+		return (
+			<div className="signup-container">
+				<div className="signup-left">
+					<div className="flow-container">
+						<div className="flow-circle">
+							<img src={File} alt="" />
+							<p className="mt-2">Create your grocery list</p>
+						</div>
+						<img className="mx-4" src={Arrow} alt="" />
+						<div className="flow-circle">
+							<img src={Truck} alt="" />
+							<p className="mt-2">
+								Receive your groceries Weekly
+							</p>
+						</div>
+					</div>
+					<h2 className="its-simple">It’s that simple!</h2>
+				</div>
+				<SignUpForm onSubmit={this.onSignUpSubmit} />
+				{/* <Link to="/login">Login</Link> */}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    return ({
-        _id: state.userReducer._id,
-        firstName: state.userReducer.firstName
-    })
-}
-
+	return {
+		_id: state.userReducer._id,
+		firstName: state.userReducer.firstName,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => ({
-        userLoggedOn: (userData) => dispatch(userLoggedIn(userData)) 
-    })
+	userLoggedOn: (userData) => dispatch(userLoggedIn(userData)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Signup);
