36932b1ff10a5ed7ce2868b431a7c19c86dc3be8
Avinash Rathee <67560625+gazingintolife@users.noreply.github.com>
2021-09-27 11:57:18 -0400

    Contact Form Added

---
+++ server/controller/contact.js
@@ -0,0 +1,7 @@
+// const nodemailer = require('nodemailer')
+
+// const sendCustomerQuery = async (req, res) => {
+//     let transporter = nodemailer.createTransport({
+//         host: ""
+//     }, [defaults])
+// }
--- src/app/components/Pages/contact/contact.js
+++ src/app/components/Pages/contact/contact.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Button } from '../../button/button';
 
-const contact = () => {
-    return (
-        <div>
-            <h1> This is contact page </h1>
-        </div>
-    )
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            body: '',
+            err: ''
+        }
+    }
+
+    onEmailChange = () => {
+        const email = e.target.value;
+		this.setState(() => ({ email: email }));
+    }
+
+    onBodyChange = () => {
+        const body = e.target.value;
+		this.setState(() => ({ body: body }));
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (
+			!this.state.email ||
+			!this.state.body
+		) {
+			this.setState(() => ({ error: "Please fill all the fields" }));
+		} else {
+			this.setState(() => ({ error: "" }));
+            const customerQuery = {
+                email : this.state.email,
+                body : this.state.body
+            } 
+			axios.post("/api/v1/contact", customerQuery)
+            .then((res) => (console.log(res)))
+			this.setState(() => ({
+				email: "",
+				body: ""
+			}));
+		}
+    }
+    
+    render(){
+        return (
+            <div>
+                <h1> How can we help? </h1>
+                <form>
+                    <input
+                        type = "email"
+                        placeholder = "Email"
+                        value = {this.state.email}
+                        onChange = {this.onEmailChange}
+                    />
+                    <input
+                        type = "text"
+                        placeholder = "How can we help?"
+                        value = {this.state.body}
+                        onChange = {this.onBodyChange}
+                    />
+                    <Button onClick = {this.onSubmit} buttonText = "Submit"/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default contact;
