26cadcb206b89aa57127507fb16b186fc695f221
m11dedhia <m11dedhia@gmail.com>
2021-09-30 17:47:50 +0530

    added schemas for category and product

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,18 +1,38 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose, { Schema } = require('mongoose');
 
-const categorySchema = new Schema ({
-    name: {type: String, required: true},
-    parent: {type: Schema.Types.ObjectId, ref: 'category'},
-    children: [{type: Schema.Types.ObjectId, ref: 'category'}],
-    ancestors: [{type: Schema.Types.ObjectId, ref: 'category'}],
-    products: [{type: Schema.Types.ObjectId, ref: 'product'}]
-},
-{
-    collection: 'Categories',
-    timestamps: true
-}
+const categorySchema = new Schema (
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    parent: {
+      type: Schema.Types.ObjectId,
+      default: null,
+    },
+    children: {
+      type: [Schema.Types.ObjectId],
+      default: [],
+    },
+    ancestors: {
+      type: Schema.Types.ObjectId,
+      default: [],
+    },
+    products: {
+      type: Schema.Types.ObjectId,
+      default: [],
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_on',
+      updatedAt: 'updated_on',
+    },
+  }
 );
-module.exports = mongoose.model('category', categorySchema);
-
- 
+module.exports = mongoose.model('category', categorySchema, 'category');
--- server/models/product.js
+++ server/models/product.js
@@ -1,23 +1,42 @@
-import { Mongoose } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-
-const { Schema } = Mongoose;
-
-const productInfo = new Schema ({
-    name: {type: String, required: true},
-    price: {type: Number, required: true},
-    description: {type: String},
-    size: {type: String, required: true},
-    tags: [{type: String}],
-    category: {type: Schema.Types.ObjectId, ref: 'Category'},
-    status: {type: String}
-},
-{ 
-    collection: "Products",
-    timestamps: true
-}
+const productSchema = new Schema (
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    description: String,
+    // size: {type: String, required: true},
+    tags: {
+      type: [String],
+      default: [],
+    },
+    sku: {
+      type: String,
+      required: true,
+    },
+    category_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    }
+  },
+  { 
+    timestamps: {
+      createdAt: 'created_on',
+      updatedAt: 'updated_on',
+    },
+  }
 );
 
-const products = Mongoose.model('product', productInfo);
+const products = mongoose.model('product', productSchema);
 
 export default products;
